ebf897e959c4e7853c60586c9f4a4e6ebd41e8c3
Andrei Strakhov <straxarts@outlook.com>
2023-10-30 14:57:09 +0200

    корректировал код

--- Exersize64/Program.cs
+++ Exersize64/Program.cs
@@ -2,19 +2,15 @@
 //  натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+string NatNumRev (int N)
+{       
+    if (N == 1) return "1" ;
+    else return $"{N} " + NatNumRev (N-1);
+}
+
 Console.Write("Введите целое число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-if (N<1) Console.WriteLine("На данном промежутке натуральных чисел нет.");
-else
-{
-    
-    for (N=N; N>=1; N--)
-    {
-        Console.WriteLine(N);
-    }
-    // while (N>=1)
-    // {
-    //     Console.WriteLine(N);
-    //     N--;
-    // }
-}
+if (N < 1) Console.WriteLine("На данном промежутке натуральных чисел нет.");
+else Console.WriteLine(NatNumRev (N));
--- Exersize68/Program.cs
+++ Exersize68/Program.cs
@@ -2,3 +2,16 @@
 // с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+int Accerman(int Z, int X)
+{   
+    int A = 0;
+    if (Z < 0 | X < 0) return -1;
+    else if (Z == 0) return A = X + 1;
+    else if (Z > 0 & X == 0) return Accerman (Z-1, 1);
+    else return Accerman (Z-1, Accerman(Z, X - 1));   
+}
+Console.Write("Введите положительное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите положительное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Accerman (M, N));
